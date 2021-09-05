dc7f34054020f2a4ff1ce9ede6218d746682de30
Sergey <sergeysuyazov2009@yandex.ru>
2021-09-06 00:17:52 +0300

    Added support for "More miniloaders" mod

--- data.lua
+++ data.lua
@@ -16,3 +16,27 @@ end
 if data.raw["underground-belt"]["express-underground-belt"] then
     data.raw["underground-belt"]["express-underground-belt"].next_upgrade = "uranium-underground-belt"
 end
+
+if mods["more-miniloaders"] then 
+    more_miniloaders.create_miniloader{
+        name = "uranium",
+        color = "43ff4b",
+        ingredients = {
+            {"express-miniloader", 1}, 
+            {"uranium-underground-belt", 1}, 
+            {"stack-inserter", 4}, 
+        },
+        filter_ingredients = {
+            {"express-filter-miniloader", 1}, 
+            {"uranium-underground-belt", 1}, 
+            {"stack-filter-inserter", 4}, 
+        },
+        tech_prereq = {"uranium-logistics", "express-miniloader"},
+    }
+
+    more_miniloaders.modify_miniloader{
+        name = "express", 
+        next_upgrade = "uranium",
+        fix_description = true,
+    }
+end
